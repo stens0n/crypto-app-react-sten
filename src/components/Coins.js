eaf0f55b81656a07e91e37bbab747d4e53313254
stens0n <astenson010@gmail.com>
2022-12-11 12:53:09 -0500

    coin mapping functions added

--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CoinItem from './CoinItem';
 
 const Coins = (props) => {
   return (
@@ -12,6 +13,14 @@ const Coins = (props) => {
                 <p className="hide-mobile">Volume</p>
                 <p className="hide-mobile">Mkt Cap</p>
             </div>
+
+            {props.coins.map(coins => {
+                return (
+                    <CoinItem coins={coins} />
+                )
+            })}
+
+
         </div>
     </div>
   )
