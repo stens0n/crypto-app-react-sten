a82d6457ed37b8e69073161cf59df2841bcf6a22
stens0n <astenson010@gmail.com>
2022-12-13 22:07:36 -0500

    styling updates

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
 
   const [coins, setCoins] = useState([])
   
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=11&page=1&sparkline=false'
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false'
 
 
   useEffect(() => {
     axios.get(url).then((response) => {
       setCoins(response.data)
-      console.log(response.data[0])
+      //console.log(response.data[0])
     }).catch((error) => {
       console.log(error)
     })
